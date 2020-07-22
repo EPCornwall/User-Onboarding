0e2f195406a42ade052d0c79330185d6f698ebb9
EPCornwall <Elizabeth.cornwall91@gmail.com>
2020-07-22 16:08:18 -0500

    added form skeleton

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
+import UserList from './components/UserList'
+
+
+const initialFormValue = {
+  name:'',
+  email:'',
+  password:'',
+  //TOS checkbox
+}
+const initialUsers = []
 
 function App() {
+  const [form, setForm] = useState(initialFormValue)
+  const [users, setUsers] = useState(initialUsers)
   return (
     <div className="App">
-     <Form />
+     <Form form={form}/>
+     <UserList/>
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default function Form(props){
-
+    const {handleChange, form, handleSubmit} = props
     return(
-        <div>Form</div>
+        <div>
+            <form>
+                <input
+                    placeholder='Name'
+                    name='name'
+                    value={form.name}
+                    onChange={(e)=>{handleChange(e)}}
+                />
+                
+            </form>
+        </div>
     )
         
 }
